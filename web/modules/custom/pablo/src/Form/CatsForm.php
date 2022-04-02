f99486ceeb32b6576753e97e77c9f1c14295627c
Pablo <pablopukha@gmail.com>
2022-04-02 14:15:42 +0300

    added form submit AJAX

--- web/modules/custom/pablo/src/Form/CatsForm.php
+++ web/modules/custom/pablo/src/Form/CatsForm.php
@@ -7,6 +7,8 @@
 
 namespace Drupal\pablo\Form;
 
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\MessageCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
@@ -18,6 +20,11 @@ class CatsForm extends FormBase{
 
   public function buildForm(array $form, FormStateInterface $form_state) {
 
+    $form['system_messages'] = [
+      '#markup' => '<div id="form-system-messages"></div>',
+      '#weight' => -100,
+    ];
+
     $form['name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your cat’s name:'),
@@ -26,7 +33,7 @@ class CatsForm extends FormBase{
         'class' => [
           'form-name'
         ]
-      ]
+      ],
     ];
 
     // Add a submit button that handles the submission of the form.
@@ -37,20 +44,37 @@ class CatsForm extends FormBase{
         'class' => [
           'form-submit'
         ]
+      ],
+      '#ajax'=> [
+        'callback' => '::ajaxSubmitCallback',
+        'event' => 'click',
+        'progress' => [
+          'type' => 'throbber',
+        ],
       ]
     ];
 
     return $form;
   }
 
+  public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
+    $ajax_response = new AjaxResponse();
+
+    if(strlen($form_state->getValue('name')) < 2 || strlen($form_state->getValue('name')) > 32){
+      $ajax_response->addCommand(new MessageCommand('The minimum length of the name is 2 characters, and the maximum is 32', '#form-system-messages', ['type' => 'error']));
+    }
+    else{
+      $ajax_response->addCommand(new MessageCommand('Thank you very much for your message. The input data is valid',  '#form-system-messages', ['type' => 'status']));
+    }
+    return $ajax_response;
+  }
+
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    \Drupal::messenger()->addMessage(t('Thank you very much for your message. The input data is valid'), 'status');
+
   }
 
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (strlen($form_state->getValue('name')) < 2 || strlen($form_state->getValue('name')) > 32)  {
-      $form_state->setErrorByName('name', $this->t('The minimum length of the name is 2 characters, and the maximum is 32'));
-    }
+
   }
 
 }
