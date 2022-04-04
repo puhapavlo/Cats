40f6f733179c79ea298af25b13a7c812bde241ab
Pablo <pablopukha@gmail.com>
2022-04-04 18:29:05 +0300

    added validation email

--- web/modules/custom/pablo/src/Form/CatsForm.php
+++ web/modules/custom/pablo/src/Form/CatsForm.php
@@ -8,6 +8,7 @@
 namespace Drupal\pablo\Form;
 
 use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CssCommand;
 use Drupal\Core\Ajax\MessageCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -37,14 +38,21 @@ class CatsForm extends FormBase{
     ];
 
     $form['email'] = [
-      '#type' => 'email',
+      '#type' => 'textfield',
       '#title' => $this->t('Your email:'),
-      '#description' => $this->t('The name can only contain Latin letters, underscores or hyphens.'),
+      '#description' => $this->t('The email can only contain Latin letters, underscores or hyphens.'),
       '#required' => TRUE,
       '#attributes' => [
         'class' => [
           'form-email'
         ]
+      ],
+      '#ajax' => [
+        'callback' => '::validateEmailAjax',
+        'event' => 'input',
+        'progress' => array(
+          'type' => 'throbber',
+        ),
       ],
     ];
 
@@ -73,10 +81,29 @@ class CatsForm extends FormBase{
     $ajax_response = new AjaxResponse();
 
     if(strlen($form_state->getValue('name')) < 2 || strlen($form_state->getValue('name')) > 32){
-      $ajax_response->addCommand(new MessageCommand('The minimum length of the name is 2 characters, and the maximum is 32', '#form-system-messages', ['type' => 'error']));
+      $ajax_response->addCommand(new MessageCommand($this->t('The minimum length of the name is 2 characters, and the maximum is 32'), '#form-system-messages', ['type' => 'error']));
+    }
+
+    elseif(!preg_match('/^[a-z-_]+$/i', $form_state->getValue('email'))){
+      $ajax_response->addCommand(new MessageCommand($this->t('The email can only contain Latin letters, underscores or hyphens.'),  '#form-system-messages', ['type' => 'error'], TRUE));
+      $ajax_response->addCommand(new CssCommand('.form-email', ['border' => '2px solid red']));
+    }
+
+    else{
+      $ajax_response->addCommand(new MessageCommand($this->t('Thank you very much for your message. The input data is valid'),  '#form-system-messages', ['type' => 'status']));
+    }
+    return $ajax_response;
+  }
+
+  public function validateEmailAjax(array &$form, FormStateInterface $form_state) {
+    $ajax_response = new AjaxResponse();
+    $patternEmail = '/^[a-z-_]+$/i';
+    if (preg_match($patternEmail, $form_state->getValue('email'))) {
+      $ajax_response->addCommand(new MessageCommand($this->t('Email is valid'),  '#form-system-messages', ['type' => 'status'], TRUE));
     }
     else{
-      $ajax_response->addCommand(new MessageCommand('Thank you very much for your message. The input data is valid',  '#form-system-messages', ['type' => 'status']));
+      $ajax_response->addCommand(new MessageCommand($this->t('The email can only contain Latin letters, underscores or hyphens.'),  '#form-system-messages', ['type' => 'error'], TRUE));
+      $ajax_response->addCommand(new CssCommand('.form-email', ['border' => '2px solid red']));
     }
     return $ajax_response;
   }
